a0f2464f01aad2e366500de8918e20476f29d015
andgarzonmal <andgarzonmal@unal.edu.co>
2022-06-25 15:42:50 -0500

    delete and add react components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Test from './Components/Test.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='principal-container'>
+        <h1>Here is what our alumni say about freeCodeCamp:</h1>
+        <Test />
+      </div>
     </div>
   );
 }
