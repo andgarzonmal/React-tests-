fbb27ca28d01d3e31d9a74729c4bc6fdf4854cf1
andgarzonmal <andgarzonmal@unal.edu.co>
2022-06-26 11:04:50 -0500

    made some changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,30 @@ function App() {
     <div className="App">
       <div className='principal-container'>
         <h1>Here is what our alumni say about freeCodeCamp:</h1>
-        <Test />
+        <Test 
+          name ='Shawn Wang'
+          country = 'Singapore'
+          image = 'Shawn'
+          charge = 'Software Engineer'
+          company = 'Amazon'
+          test = "It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+        />
+        <Test 
+          name ='Sarah Chima'
+          country = 'Nigeria'
+          image = 'Sara'
+          charge = 'Software Engineer'
+          company = 'ChatDesk'
+          test = "freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
+        />
+        <Test 
+          name ='Emma Bostian'
+          country = 'Sweden'
+          image = 'emma'
+          charge = 'Software Engineer'
+          company = 'Spotify'
+          test = "I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+        />
       </div>
     </div>
   );
--- my-app/src/Components/Test.js
+++ my-app/src/Components/Test.js
@@ -1,17 +1,17 @@
 import react from 'react';
 import '../style/Test.css'
 
-const Test = () => {
+const Test = (props) => {
   return (
     <div className='test-cont'>
       <img 
-        className='test-img' src={require('../images/test-emma.png')}
+        className='test-img' src={require(`../images/test-${props.image}.png`)}
         alt='Emmas photo'
         />
       <div className='test-cont-text'>
-        <p className='test-name'>Emma Bostian in Sweden</p>
-        <p className='test-charge'>Software Engineer at Spotify</p>
-        <p className='test-text'>"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+        <p className='test-name'>{props.name} in {props.country}</p>
+        <p className='test-charge'>{props.charge} at {props.company}</p>
+        <p className='test-text'>{props.test}</p>
       </div>
     </div>
   );
